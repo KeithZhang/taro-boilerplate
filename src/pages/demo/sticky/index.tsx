2216b99e97542e5bd2623dc9728eb3d3c5832eda
keithz <keithzhf@gmail.com>
2020-01-14 16:55:00 +0800

    sticky

--- src/pages/demo/sticky/index.tsx
+++ src/pages/demo/sticky/index.tsx
@@ -4,8 +4,11 @@ import Sticky from "ui-base/sticky";
 import QMButton from "ui-base/button";
 
 import "./index.module.less";
+import { useState } from "@tarojs/taro";
 
 export default function StickyDemo() {
+  const container = () => Taro.createSelectorQuery().select("#container");
+
   return (
     <View>
       <DemoBlock title="基本用法">
@@ -14,6 +17,24 @@ export default function StickyDemo() {
             基本用法
           </QMButton>
         </Sticky>
+      </DemoBlock>
+
+      <DemoBlock title="吸顶距离">
+        <Sticky offsetTop={50}>
+          <QMButton type="info" customStyle="margin-left: 115px">
+            吸顶距离
+          </QMButton>
+        </Sticky>
+      </DemoBlock>
+
+      <DemoBlock title="指定容器">
+        <View id="container" style="height: 150px; background-color: red;">
+          <Sticky container={container}>
+            <QMButton type="warning" customStyle="margin-left: 215px">
+              指定容器
+            </QMButton>
+          </Sticky>
+        </View>
       </DemoBlock>
     </View>
   );
